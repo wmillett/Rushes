0e802d9eb1c3124d483c037158487e769f6170f1
wmillett <williammillette98@gmail.com>
2025-07-05 14:57:57 +0200

    added basic formating for makefile and project

---
+++ Retromfa/include/retromfa.h
@@ -0,0 +1,16 @@
+#ifndef RETROMFA_H
+# define RETROMFA_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ Retromfa/src/main.c
@@ -0,0 +1,19 @@
+#include "retromfa.h"
+
+
+int main(int argc, char *argv[]) {
+    // Check if the correct number of arguments is provided
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <MFA files>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    // Process each MFA file provided as an argument
+    for (int i = 1; i < argc; i++) {
+        printf("Processing file: %s\n", argv[i]);
+        // Here you would add the logic to handle each MFA file
+        // For now, we just print the filename
+    }
+
+    return EXIT_SUCCESS;
+}
